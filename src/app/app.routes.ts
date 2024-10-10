26fd6f3339635c47bb27c2bca03abca06d181ddf
LEONARDO CARVALHO SILVA <leonardo.carvalho@uniggelsementes.com.br>
2024-10-10 17:45:11 -0300

    criando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { EstadoFormComponent } from './components/estado/estado-form/estado-form.component';
+import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
+import { estadoResolver } from './components/estado/resolver/estado.resolver';
+import { MunicipioFormComponent } from './components/municipio/municipio-form/municipio-form.component';
+import { MunicipioListComponent } from './components/municipio/municipio-list/municipio-list.component';
+import { municipioResolver } from './components/municipio/resolver/municipio.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '', pathMatch: 'full'},
+
+    {path: 'estados', component: EstadoListComponent, title: 'Lista de Estados'},
+    {path: 'estados/new', component: EstadoFormComponent, title: 'Novo Estado'},
+    {path: 'estados/edit/:id', component: EstadoFormComponent, resolve: {estado: estadoResolver}},
+
+    {path: 'municipios', component: MunicipioListComponent, title: 'Lista de municipios'},
+    {path: 'municipios/new', component: MunicipioFormComponent, title: 'Novo municipio'},
+    {path: 'municipios/edit/:id', component: MunicipioFormComponent, resolve: {municipio: municipioResolver}},
+
+];
